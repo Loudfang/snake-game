be8580e686be5f5832921e19fc54ac12e8aaf014
Loudfang <ibarkat@gmx.com>
2015-11-30 04:00:34 +0400

    Building a snake game
    Building my own snake game using arrays and colors to represent the
    snake and food

---
+++ Snake/src/snake/GameBoard.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snake;
+
+import java.awt.*;
+
+/**
+ *
+ * @author Madduck
+ */
+public class GameBoard {
+    
+    public static final int TILE_SIZE = 25;
+    public static final int MAP_SIZE = 20;
+    private tiletype[] tiles;
+    
+    public static enum tiletype{
+        
+        SNAKE(Color.GREEN),
+        
+        FRUIT(Color.RED),
+        
+        /**
+         * tiles without anything in them
+         */
+        EMPTY(null);
+        
+        private Color tilecolor;
+        
+        private tiletype(Color color){
+            this.tilecolor = color;
+        }
+            
+        public Color getColor(){
+            return tilecolor;
+        }
+    }
+    
+    public GameBoard(){
+        /**
+         * Resets the board and creates new instance of empty tiles
+         */
+        tiles = new tiletype [MAP_SIZE * TILE_SIZE];
+        resetBoard();
+    }
+    
+    public void resetBoard(){
+        for(int i = 0; i <= tiles.length;i++){
+            tiles[i] = tiletype.EMPTY;
+       }
+    }
+    
+    public void setTile(int x,int y,tiletype type){
+        tiles [y* MAP_SIZE + x] = type;        
+    }
+  
+    
+}
---
+++ Snake/src/snake/TheEngine.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snake;
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+
+
+/**
+ *
+ * @author Madduck
+ */
+
+ public class TheEngine {
+     
+     private Canvas canvas;
+    private static final int UPDATES_PER_SECOND = 10;
+    
+    
+    public static void main (String [] args){
+        JFrame frame = new JFrame("Snake ");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        
+        Canvas canvas = new Canvas();
+        canvas.setBackground(Color.BLACK);
+        canvas.setPreferredSize(new Dimension(GameBoard.MAP_SIZE * GameBoard.TILE_SIZE, GameBoard.MAP_SIZE * GameBoard.TILE_SIZE));
+        frame.add(canvas);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        
+        TheEngine engine = new TheEngine(canvas);
+        engine.startGame();
+    }
+
+    private TheEngine (Canvas canvas){
+        
+      this.canvas = canvas;  
+    }
+    
+    
+    public void startGame(){
+        
+        canvas.createBufferStrategy(2);
+        Graphics2D g = (Graphics2D)canvas.getBufferStrategy().getDrawGraphics();
+        
+        long start = 0L;
+        long sleepDuration = 0L;
+        
+        while(true){
+            start = System.currentTimeMillis();
+            update();
+            render(g);
+            canvas.getBufferStrategy().show();
+            g.clearRect(0,0, canvas.getWidth(), canvas.getHeight());
+            sleepDuration = (1000L / UPDATES_PER_SECOND) - (System.currentTimeMillis()-start);
+            
+            if(sleepDuration > 0){
+                try{
+                    Thread.sleep(sleepDuration);
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+                
+            }
+        }
+    }
+ 
+        private void update(){
+               // TODO 
+
+    }
+ 
+        private void render(Graphics2D g){
+            
+            //TODO
+        }
+ 
+ }
+    
+
+
+
